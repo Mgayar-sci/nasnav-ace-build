960c37889ce74eb417ee4977964cc600ad0d6956
bftawfik <bftawfik@gmail.com>
2019-09-07 16:47:46 +0200

    finished page

---
+++ precache-manifest.94d2606099726c7608c1e9e545221393.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e573f8c9dbb3a30cbb6da94823edad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89a5f002fa08da93631d",
+    "url": "./static/css/main.27476bfa.chunk.css"
+  },
+  {
+    "revision": "eafaa344e5e95ce493ea",
+    "url": "./static/js/2.bd99587b.chunk.js"
+  },
+  {
+    "revision": "89a5f002fa08da93631d",
+    "url": "./static/js/main.7fe5cbd9.chunk.js"
+  },
+  {
+    "revision": "14b2642fbedb48c5e90a",
+    "url": "./static/js/runtime~main.9f250986.js"
+  },
+  {
+    "revision": "358eb6ba612af8ae832e949a965d422c",
+    "url": "./static/media/360-background.358eb6ba.jpg"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "./static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "cec108b9d479fba6f69621daabc4ba1a",
+    "url": "./static/media/ace-logo-2.cec108b9.png"
+  },
+  {
+    "revision": "6b93eba33cb78aa78f44d0d5abdb7dce",
+    "url": "./static/media/branch1.6b93eba3.jpg"
+  },
+  {
+    "revision": "64f0725f14e265bb386e1114e7ddf29b",
+    "url": "./static/media/branch2.64f0725f.jpg"
+  },
+  {
+    "revision": "599e1f2f46032a235ec84f0335f45102",
+    "url": "./static/media/ego-left.599e1f2f.jpg"
+  },
+  {
+    "revision": "790d5f8e169c14ccead6fee31c2559ab",
+    "url": "./static/media/ego-right.790d5f8e.jpg"
+  },
+  {
+    "revision": "26e268bd64af4d02d16320747ba54de6",
+    "url": "./static/media/slideImage1.26e268bd.jpg"
+  }
+]);
